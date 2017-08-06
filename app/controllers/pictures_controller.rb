c688f867dcffea0a98ba003ca5264607ebb60f82
Benny Back <email@example.com>
2017-08-06 18:32:10 -0400

    more comment changes

--- app/controllers/pictures_controller.rb
+++ app/controllers/pictures_controller.rb
@@ -10,6 +10,7 @@ class PicturesController < ApplicationController
   # GET /pictures/1
   # GET /pictures/1.json
   def show
+     @comment = Comment.new( :picture => @picture )
   end
 
   # GET /pictures/new
